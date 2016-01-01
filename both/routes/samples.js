61528d3d7deb8c864d1e5d605f0a60e66efcc0b5
zarazi <zarazi@hotmail.com>
2016-01-02 06:48:37 +0700

    Refactor samples route.

--- both/routes/samples.js
+++ both/routes/samples.js
@@ -1,4 +1,8 @@
-FlowRouter.route('/sample-overlay', {
+const samplesSection = FlowRouter.group({
+    name: 'samples'
+});
+
+samplesSection.route('/sample-overlay', {
     name: 'sampleOverlay',
     action() {
         BlazeLayout.render('defaultOverlay', {main: 'sampleOverlayPage'});
