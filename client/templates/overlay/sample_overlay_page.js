072bdea69d8e62208711b546598e568334c7316b
zarazi <zarazi@hotmail.com>
2016-01-02 07:08:42 +0700

    Refactor className, fix overlay sample text color.

--- client/templates/overlay/sample_overlay_page.js
+++ client/templates/overlay/sample_overlay_page.js
@@ -1,12 +1,12 @@
 Template.sampleOverlayPage.events({
     'click .btn-show-overlay': function(event, template) {
-        $('.app-layout').addClass('searching');
+        $('.app-layout').addClass('show-overlay');
     },
 
     'keyup': function(event, template) {
         //console.log(event.which);
         if (event.which===27) {
-            $('.app-layout').removeClass('searching');
+            $('.app-layout').removeClass('show-overlay');
         }
     }
 });
